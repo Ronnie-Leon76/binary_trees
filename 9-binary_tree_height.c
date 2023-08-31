3222fd5b463ab3c258dc38cf02dfaceaadab64ad
Ronnie-Leon76 <ronleon76@gmail.com>
2023-08-31 23:31:02 +0300

    feat: fixed issue with binary_tree_height function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,16 +9,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
 	if (!tree)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+	else if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+	else
+		return (0);
+	return (left_height > right_height ? left_height : right_height);
 
-	if (left_height > right_height)
-		return (left_height + 1);
-
-	return (right_height + 1);
 }
